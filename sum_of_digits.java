f6af0a7f9e7bf62bfeff8f67424e6734a88571c0
Harsh0166 <2005harshkumar@gmail.com>
2025-11-11 13:12:38 +0530

    up

--- sum_of_digits.java
+++ sum_of_digits.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class sum_of_digits {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int digit = sc.nextInt();
+        int sum = 0 ;
+        while(digit>0){
+            int rem = digit%10;
+            sum = sum + rem;
+            digit = digit/10;
+        }
+        System.out.println(sum);
+
+
+        
+    } 
 }
